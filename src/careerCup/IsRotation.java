b05257d91aad14a9116a3d330c3a835381832e6b
jingz8804 <jingz8804@gmail.com>
2014-05-01 08:03:44 -0600

    Update IsRotation.java
    also check the null value at the beginning

--- src/careerCup/IsRotation.java
+++ src/careerCup/IsRotation.java
@@ -8,14 +8,11 @@ public class IsRotation {
 		System.out.println("is str2 a rotation of str1? " + isRotation(str1, str2));
 		String str3 = "cdab";
 		System.out.println("is str3 a rotation of str1? " + isRotation(str1, str3));
-		int a = 11;
-		System.out.println(Integer.toBinaryString(a));
-		System.out.println(Integer.toBinaryString(~a));
 	}
 	
 	private static boolean isRotation(String str1, String str2){
-		String combined = str1 + str1;
-		if (str1.length == str2.length && combined.indexOf(str2) > 0) return true;
+		if(str1 == null or str2 == null) return false;
+		if (str1.length == str2.length && (str1 + str1).indexOf(str2) > 0) return true;
 		return false;
 	}
 }
